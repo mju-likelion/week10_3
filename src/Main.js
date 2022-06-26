2e5404c8194aa17dbf8971894e292434acc58453
yuraup <o721@naver.com>
2022-06-26 16:59:25 +0900

    메인 페이지 UI 구현

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,32 @@
-const Main = ()=>{
-    return (<p>main 화면입니당.</p>)
-}
+import "../src/Main.css";
+
+const Main = () => {
+  return (
+    <div className="MainWrapper">
+      <div className="TitleBox">
+        <p>멋쟁이 사자처럼(명지대)</p>
+      </div>
+      <div className="TeamBox">
+        <button type="button" className="buttonStyle">
+          대표
+        </button>
+        <button type="button" className="buttonStyle">
+          운영팀
+        </button>
+        <button type="button" className="buttonStyle">
+          교육팀
+        </button>
+        <button type="button" className="buttonStyle">
+          홍보팀
+        </button>
+        <button type="button" className="buttonStyle">
+          개발팀
+        </button>
+      </div>
+      <p className="pStyle">안녕하세요 3조입니다!💖</p>
+      <p className="pStyle2">어느 팀을 구경하시겠습니까?</p>
+    </div>
+  );
+};
 
 export default Main;
