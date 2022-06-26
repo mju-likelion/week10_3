ce5ce7d9a813c331f7f7601958b2e731a3d7ce99
yuraup <o721@naver.com>
2022-06-26 17:23:39 +0900

    link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Main from './Main';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Main from "./Main";
+import Manage from "../src/pages/Manage";
+import Education from "../src/pages/Education";
+import President from "../src/pages/President";
+import Promotion from "../src/pages/Promotion";
+import Development from "./pages/Development";
 function App() {
-  return ( 
+  return (
     <Routes>
-  <Route path='/' element={<Main />}/>
-  </Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/Development" element={<Development />} />
+      <Route path="/Manage" element={<Manage />} />
+      <Route path="/Education" element={<Education />} />
+      <Route path="/President" element={<President />} />
+      <Route path="/Promotion" element={<Promotion />} />
+    </Routes>
   );
 }
 
