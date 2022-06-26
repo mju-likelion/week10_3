66353bb5b15fb43f6bf1ce98305e7a41e6c7a853
yuraup <o721@naver.com>
2022-06-26 17:10:25 +0900

    메인 페이지 UI 디자인 수정

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import "../src/Main.css";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
@@ -7,21 +8,31 @@ const Main = () => {
         <p>멋쟁이 사자처럼(명지대)</p>
       </div>
       <div className="TeamBox">
-        <button type="button" className="buttonStyle">
-          대표
-        </button>
-        <button type="button" className="buttonStyle">
-          운영팀
-        </button>
-        <button type="button" className="buttonStyle">
-          교육팀
-        </button>
-        <button type="button" className="buttonStyle">
-          홍보팀
-        </button>
-        <button type="button" className="buttonStyle">
-          개발팀
-        </button>
+        <Link to="/President">
+          <button type="button" className="buttonStyle">
+            대표
+          </button>
+        </Link>
+        <Link to="/Manage">
+          <button type="button" className="buttonStyle">
+            운영팀
+          </button>
+        </Link>
+        <Link to="/Education">
+          <button type="button" className="buttonStyle">
+            교육팀
+          </button>
+        </Link>
+        <Link to="/Promotion">
+          <button type="button" className="buttonStyle">
+            홍보팀
+          </button>
+        </Link>
+        <Link to="/Development">
+          <button type="button" className="buttonStyle">
+            개발팀
+          </button>
+        </Link>
       </div>
       <p className="pStyle">안녕하세요 3조입니다!💖</p>
       <p className="pStyle2">어느 팀을 구경하시겠습니까?</p>
