a21ae12f0644108e4821d4a9a5884896d2f525ae
Jieun Lee <najee0610@naver.com>
2022-06-26 16:33:45 +0900

    first commit 파일구조 변경

---
+++ src/Main.js
@@ -0,0 +1,5 @@
+const Main = ()=>{
+    return (<p>main 화면입니당.</p>)
+}
+
+export default Main;
---
+++ src/pages/President.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const President = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default President;
